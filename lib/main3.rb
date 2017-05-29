61b4a53698b6407cdd1937d3d392e516f27d3d3e
Filipe Esperandio <filipesperandio@gmail.com>
2017-05-29 18:31:07 -0300

    Revert "improve tests"
    This reverts commit 722394c324d938c9e808d153a828130d4477b0f5.

--- lib/main3.rb
+++ lib/main3.rb
@@ -4,6 +4,65 @@ module Main3
   def useless2(method)
     puts "bullshit"
     puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+  end
+
+  def useless(method)
+    puts "bullshit"
   end
 
   def useless_big(method)
@@ -12,16 +71,76 @@ module Main3
     end
   end
 
-  def useless1(method)
+  def useless(method)
     if method
       if !method
         if !method
-          puts '231'
+          if !method
+            if !method
+              if method
+                if !method
+                  if !method
+                    if !method
+                      if !method
+                        puts "bullshit"
+                      end
+                    end
+                  end
+                end
+              end
+            end
+          end
         end
       end
     end
   end
 
+  def useless(method)
+    if method
+      if !method
+        if !method
+          if !method
+            if !method
+              if method
+                if !method
+                  if !method
+                    if !method
+                      if !method
+                        puts "bullshit"
+                      end
+                    end
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
   
+  def useless(method)
+    if method
+      if !method
+        if !method
+          if !method
+            if !method
+              if method
+                if !method
+                  if !method
+                    if !method
+                      if !method
+                        puts "bullshit"
+                      end
+                    end
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
 end
 
---
+++ spec/lib/main3_spec.rb
@@ -0,0 +1,11 @@
+# encoding: UTF-8
+require 'spec_helper'
+
+describe Main3 do
+
+  it 'test something' do
+    subject.useless_big "something"
+  end
+
+end
+
