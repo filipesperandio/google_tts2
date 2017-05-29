fc8dea661a249b9d656d27e698155e159a7b8405
Filipe Esperandio <filipesperandio@gmail.com>
2017-05-29 13:39:24 -0300

    New module

--- lib/google_tts/proxy_fetcher.rb
+++ lib/google_tts/proxy_fetcher.rb
@@ -10,17 +10,6 @@ module GoogleTts
     ]
 
     def self.list
-      list = []
-      @@SOURCES.each do |url|
-        Nokogiri::XML(open url).xpath("//prx:proxy").each do |node|
-          proxy = { host: node.xpath("prx:ip").text, 
-                    port: node.xpath("prx:port").text,
-                    type: node.xpath("prx:type").text,
-                    ssl: node.xpath("prx:ssl").text.downcase == "true" }
-          list << proxy if (proxy[:type] == "Anonymous" and not proxy[:ssl])
-        end
-      end
-      list
     end
 
     def self.random_proxy
