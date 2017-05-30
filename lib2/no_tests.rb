ac91510484e3522590f8b04524adb9b9d605551d
Filipe Esperandio <filipesperandio@gmail.com>
2017-05-30 17:03:51 -0300

    file not loaded

---
+++ lib2/no_tests.rb
@@ -0,0 +1,147 @@
+module NoTests
+  UNUSED_VAR = 123
+
+  def useless2(method)
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+    puts "bullshit"
+  end
+
+  def useless(method)
+    puts "bullshit"
+  end
+
+  def useless_big(method)
+    if method
+      puts "123123"
+    end
+  end
+
+  def useless(method)
+    if method
+      if !method
+        if !method
+          if !method
+            if !method
+              if method
+                if !method
+                  if !method
+                    if !method
+                      if !method
+                        puts "bullshit"
+                      end
+                    end
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+
+  def useless(method)
+    if method
+      if !method
+        if !method
+          if !method
+            if !method
+              if method
+                if !method
+                  if !method
+                    if !method
+                      if !method
+                        puts "bullshit"
+                      end
+                    end
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+  
+  def useless(method)
+    if method
+      if !method
+        if !method
+          if !method
+            if !method
+              if method
+                if !method
+                  if !method
+                    if !method
+                      if !method
+                        puts "bullshit"
+                      end
+                    end
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+
