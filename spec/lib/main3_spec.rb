a3c4976281dff9c4bcf9fc012880f979de666f28
Filipe Esperandio <filipesperandio@gmail.com>
2017-05-30 16:43:22 -0300

    improve tests

--- spec/lib/main3_spec.rb
+++ spec/lib/main3_spec.rb
@@ -1,10 +1,16 @@
 # encoding: UTF-8
 require 'spec_helper'
 
+class Test
+  include Main3
+end
+
 describe Main3 do
 
   it 'test something' do
-    subject.useless_big "something"
+    test = Test.new
+    test.useless_big "something"
+    test.useless2 'somthing'
   end
 
 end
